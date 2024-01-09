589a88698641a74dd9ecb308c2802d0a68ceb640
井上佳奈 <inouekana@jingshanaideMBP.AirPort>
2024-01-09 11:54:16 +0900

    Add existing file

---
+++ redirectURL.js
@@ -0,0 +1,10 @@
+const redirectURL = [
+    'https://forms.gle/3vCGYyWFx4oeXFq26', 
+    'https://forms.gle/nsUjMY5EptVhoXfZ6',
+    'https://forms.gle/GTsXPBciiPzTeEzu5', 
+    'https://forms.gle/FgA1R8WfBgU6t4rN8',
+    'https://forms.gle/89FB32hD2ymzJFAWA',
+    'https://forms.gle/aP5KP6bxjTYL7ygc6',
+    'https://forms.gle/jKGLPVP12iYJwRLK9',
+    'https://forms.gle/22FNbvLBr372Vsys7'
+];
---
+++ script.js
@@ -0,0 +1,33 @@
+function redirect(){
+    var phoneInput = document.getElementById("phoneInput");
+    if (!phoneInput) {
+        console.error("无法找到输入框元素。");
+        return;
+    }
+    var lastFourDigits = phoneInput.value;
+
+    if (lastFourDigits.length !== 4 || isNaN(lastFourDigits)) {
+        alert("请输入有效的手机后四位数字。");
+        return;
+    }
+    var redirectURL = [
+        'https://forms.gle/3vCGYyWFx4oeXFq26', 
+        'https://forms.gle/nsUjMY5EptVhoXfZ6',
+        'https://forms.gle/GTsXPBciiPzTeEzu5', 
+        'https://forms.gle/FgA1R8WfBgU6t4rN8',
+        'https://forms.gle/89FB32hD2ymzJFAWA',
+        'https://forms.gle/aP5KP6bxjTYL7ygc6',
+        'https://forms.gle/jKGLPVP12iYJwRLK9',
+        'https://forms.gle/22FNbvLBr372Vsys7'
+    ];
+    //对输入的四位数进行取余，并使其范围在1~8之间
+    var remainder =(lastFourDigits % redirectURL.length) + 1;
+    
+    //var randomPageNumber = Math.floor(Math.random()*8)+1;
+
+    // 构造跳转链接
+    //var redirectURL = "https//example.com/page" + randomPageNumber + ".html";
+
+    //执行跳转
+    window.location.href = redirectURL.length > 0 ? redirectURL[remainder] : 'https://www.google.com';
+}
